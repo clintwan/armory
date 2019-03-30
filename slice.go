323f31aad1dbe033a440ffcaa1b9a0443dde5b9b
Clint Wan <wantg78@gmail.com>
2019-03-30 08:47:21 +0800

    no message

--- slice.go
+++ slice.go
@@ -1,6 +1,7 @@
 package armory
 
 import (
+	"errors"
 	"reflect"
 )
 
@@ -22,19 +23,22 @@ func (arr Slice) IndexOf(ele interface{}) int {
 */
 
 // IndexOf IndexOf
-func (s *slice) IndexOf(params ...interface{}) int {
+func (s *slice) IndexOf(params ...interface{}) (int, error) {
 	arr := reflect.ValueOf(params[0])
 	v := reflect.ValueOf(params[1])
 	var t = reflect.TypeOf(params[0]).Kind()
 
 	if t != reflect.Slice && t != reflect.Array {
-		panic("Type Error! Second argument must be an array or a slice.")
+		return -1, errors.New("Type Error! First argument must be an array or a slice")
 	}
 
 	for i := 0; i < arr.Len(); i++ {
+		if arr.Index(i).Kind() != v.Kind() {
+			return -1, errors.New("Type Error! Second argument must matched any elements in first argument")
+		}
 		if arr.Index(i).Interface() == v.Interface() {
-			return i
+			return i, nil
 		}
 	}
-	return -1
+	return -1, nil
 }
